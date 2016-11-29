b4a27b408d06302eccb913cb32a7036b55e2fee1
turbographics2000 <turbographics2000@yahoo.co.jp>
2016-11-30 05:39:48 +0900

    commit

---
+++ demo.js
@@ -0,0 +1,38 @@
+navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+  preview.srcObject = stream;
+  navigator.mediaDevices.enumerateDevices().then(devices => {
+    devices.forEach(device => {
+      let btn = document.createElement('button');
+      btn.textContent = device.label;
+      btn.dataset.deviceId = device.deviceId;
+      btnContainer.appendChild(btn);
+    });
+    res.textContent = JSON.stringify(devices, null, 2);
+  });
+  function changeFacingMode(facingMode) {
+    if (preview.srcObject) {
+      preview.srcObject.getTracks().forEach(track => track.stop());
+      preview.srcObject = null;
+    }
+    navigator.mediaDevices.getUserMedia({
+      video: {
+        facingMode: { exact: facingMode },
+        optional: [{ facingMode: facingMode }]
+      }
+    }).then(stream => preview.srcObject = stream);
+  }
+  function changeDdevice(deviceId) {
+    if (preview.srcObject) {
+      preview.srcObject.getTracks().forEach(track => track.stop());
+      preview.srcObject = null;
+    }
+    navigator.mediaDevices.getUserMedia({
+      deviceId: deviceId
+    }).then(stream => preview.srcObject = stream);
+  }
+  btnUser.onclick = _ => changeFacingMode('user');
+  btnEnvironment.onclick = _ => changeFacingMode('environment');
+  btnLeft.onclick = _ => changeFacingMode('left');
+  btnRight.onclick = _ => changeFacingMode('right');
+  btnFront.onclick = _ => changeFacingMode('front');
+  btnBack.onclick = _ => changeFacingMode('back');
