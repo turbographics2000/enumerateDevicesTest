1ced97954471478601268860326feb5329a548d5
turbographics2000 <turbographics2000@yahoo.co.jp>
2016-11-30 06:03:49 +0900

    modify

--- docs/demo.js
+++ docs/demo.js
@@ -5,34 +5,38 @@ navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
       let btn = document.createElement('button');
       btn.textContent = device.label;
       btn.dataset.deviceId = device.deviceId;
+      btn.oclick = function() {
+        changeDdevice(this.dataset.deviceId);
+      }
       btnContainer.appendChild(btn);
     });
     res.textContent = JSON.stringify(devices, null, 2);
   });
-  function changeFacingMode(facingMode) {
-    if (preview.srcObject) {
-      preview.srcObject.getTracks().forEach(track => track.stop());
-      preview.srcObject = null;
-    }
-    navigator.mediaDevices.getUserMedia({
-      video: {
-        facingMode: { exact: facingMode },
-        optional: [{ facingMode: facingMode }]
-      }
-    }).then(stream => preview.srcObject = stream);
+});
+function changeFacingMode(facingMode) {
+  if (preview.srcObject) {
+    preview.srcObject.getTracks().forEach(track => track.stop());
+    preview.srcObject = null;
   }
-  function changeDdevice(deviceId) {
-    if (preview.srcObject) {
-      preview.srcObject.getTracks().forEach(track => track.stop());
-      preview.srcObject = null;
+  navigator.mediaDevices.getUserMedia({
+    video: {
+      facingMode: { exact: facingMode },
+      optional: [{ facingMode: facingMode }]
     }
-    navigator.mediaDevices.getUserMedia({
-      deviceId: deviceId
-    }).then(stream => preview.srcObject = stream);
+  }).then(stream => preview.srcObject = stream);
+}
+function changeDdevice(deviceId) {
+  if (preview.srcObject) {
+    preview.srcObject.getTracks().forEach(track => track.stop());
+    preview.srcObject = null;
   }
-  btnUser.onclick = _ => changeFacingMode('user');
-  btnEnvironment.onclick = _ => changeFacingMode('environment');
-  btnLeft.onclick = _ => changeFacingMode('left');
-  btnRight.onclick = _ => changeFacingMode('right');
-  btnFront.onclick = _ => changeFacingMode('front');
-  btnBack.onclick = _ => changeFacingMode('back');
+  navigator.mediaDevices.getUserMedia({
+    deviceId: deviceId
+  }).then(stream => preview.srcObject = stream);
+}
+btnUser.onclick = _ => changeFacingMode('user');
+btnEnvironment.onclick = _ => changeFacingMode('environment');
+btnLeft.onclick = _ => changeFacingMode('left');
+btnRight.onclick = _ => changeFacingMode('right');
+btnFront.onclick = _ => changeFacingMode('front');
+btnBack.onclick = _ => changeFacingMode('back');
